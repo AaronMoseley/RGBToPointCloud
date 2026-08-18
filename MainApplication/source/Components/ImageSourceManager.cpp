f15cc7506f07f9a4f4c0ac959493282bef414a59
Aaron Moseley <amoseley018@gmail.com>
2026-08-17 20:45:20 -0400

    Added line

--- MainApplication/source/Components/ImageSourceManager.cpp
+++ MainApplication/source/Components/ImageSourceManager.cpp
@@ -174,6 +174,7 @@ void ImageSourceManager::TriggerExportLAS(std::string filePath)
 {
 	m_imageSourceManagementWidget->SetButtonsEnabled(false);
 	m_activeThreads++;
+
 	std::thread exportThread(&ImageSourceManager::ExportLAS, this, filePath);
 	exportThread.detach();
 }
